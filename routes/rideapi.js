68eb6ed71193ad24d374e353bc55826fa6b5fdc3
Stephanie Workman <sworkman2014@gmail.com>
2016-12-22 12:11:09 -0600

    meh

--- routes/rideapi.js
+++ routes/rideapi.js
@@ -3,13 +3,20 @@ var router = express.Router();
 var Ride = require('../models/ride');
 var Drivers = require('../models/driver');
 
-
-router.get('/',function(req,res,next){
-  Ride.find({},'',function( err,rides ){
-    if(err) console.error('Error gettting posts: ', err);
-    res.json(rides);
+// GET notifications
+router.get('/notifications',function(req,res,next){
+  Drivers.find({ driverId },'notifications',function( err,notifications ){
+    if(err) console.error('Error gettting notifications: ', err);
+    res.json(notifications);
   });
 });
+
+// router.get('/', function(req, res, next) {
+// 	PassengerModel.find({}, '', function(err, allPassengers){
+// 		if(err) console.error('Error getting passenger:', err);
+// 		res.json(allPassengers);
+// 	});
+// });
 
 router.post('/requestride',function(req,res,next){
   // modify this object to have just the address key
