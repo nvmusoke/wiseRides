1769bfa309d6c57e6fd99c3aa64b77cf07b14be3
Stephanie Workman <sworkman2014@gmail.com>
2016-12-23 12:39:48 -0600

    final

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
+var DriverModel = require('../models/driver');
 
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
